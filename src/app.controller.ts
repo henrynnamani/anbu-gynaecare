efbdee44d9b66daa4967ec98554cf1ad43708fe2
henrynnamani <hoyx0101@gmail.com>
2026-02-20 08:01:42 +0100

    fix: health endpoint set to public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { SkipAuth } from './module/auth/decorator/skipAuth.decorator';
 
 @Controller()
 export class AppController {
   @Get('health')
+  @SkipAuth()
   healthCheck() {
     return {
       status: 'ok',
